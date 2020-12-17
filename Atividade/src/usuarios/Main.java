7ea0e438338dfc1046c18d324c0268094173f3f6
Julio-César-lacerda <60928218+julio-cesar-lacerda@users.noreply.github.com>
2020-12-17 21:10:02 -0200

    02

--- Atividade/src/usuarios/Main.java
+++
@@ -1,9 +0,0 @@
-package usuarios;
-
-import carros.Veiculo;
-
-public class Main{
-    public static void main(String[] args) {
-        Veiculo usuario = new Veiculo(00001,"2020",2021," Palio, da Fiat","julio");
-    }
-}
